e6ccc42df8ffb0ab2d60e0314dcc1529f72a1d03
Mung2 <101871361+Mung2@users.noreply.github.com>
2025-01-10 19:11:14 +0900

    Update rs485_2mqtt.py

--- NavienRS485/rs485_2mqtt.py
+++ NavienRS485/rs485_2mqtt.py
@@ -264,52 +264,17 @@ class Wallpad:
 wallpad = Wallpad()
 
 ## 전열교환기
-# 상태 매핑
-packet_2_payload_power = {'00': 'OFF', '01': 'ON'}
-packet_2_payload_mode = {'01': '일반', '03': '전열'}
-packet_2_payload_percentage = {'01': '1', '02': '2', '03': '3'}
-# 명령 매핑
-power_2_packet = {'OFF': '00', 'ON': '01'}
-mode_2_packet = {'일반': '01', '전열': '03'}
-percentage_2_packet = {'1': '01', '2': '02', '3': '03'}
 # 등록
 optional_info = {'optimistic': 'false', 'speed_range_min': 1, 'speed_range_max': 3}
 전열교환기 = wallpad.add_device(device_name='전열교환기', device_id='32', device_subid='01', device_class='fan', optional_info=optional_info)
 # 상태 등록
-전열교환기.register_status(
-    message_flag='81',
-    attr_name='power',
-    topic_class='state_topic',
-    regex=r'00(0[01])',
-    process_func=lambda v: packet_2_payload_power[v])
-전열교환기.register_status(
-    message_flag='81',
-    attr_name='mode',
-    topic_class='mode_state_topic',
-    regex=r'00010[123]0[13]',
-    process_func=lambda v: packet_2_payload_mode[v])
-전열교환기.register_status(
-    message_flag='81',
-    attr_name='percentage',
-    topic_class='percentage_state_topic',
-    regex=r'0001(0[1-3])',
-    process_func=lambda v: packet_2_payload_percentage[v])
+전열교환기.register_status(message_flag='81', attr_name='power', topic_class='state_topic', regex=r'00(0[01])', process_func=lambda v: 'ON' if v == '01' else 'OFF')
+전열교환기.register_status(message_flag='81', attr_name='mode', topic_class='mode_state_topic', regex=r'00010[123]0[13]', process_func=lambda v: {'01': 'bypass', '03': 'heat'}.get(v, 'off')
+전열교환기.register_status(message_flag='81', attr_name='percentage', topic_class='percentage_state_topic', regex=r'0001(0[1-3])', process_func=lambda v: {'01': '1', '02': '2', '03': '3'}.get(v, 'OFF')
 # 명령 등록
-전열교환기.register_command(
-    message_flag='41',
-    attr_name='power',
-    topic_class='command_topic',
-    process_func=lambda v: power_2_packet[v])
-전열교환기.register_command(
-    message_flag='43',
-    attr_name='mode',
-    topic_class='mode_command_topic',
-    process_func=lambda v: mode_2_packet[v])
-전열교환기.register_command(
-    message_flag='42',
-    attr_name='percentage',
-    topic_class='percentage_command_topic',
-    process_func=lambda v: percentage_2_packet[v])
+전열교환기.register_command(message_flag='41', attr_name='power', topic_class='command_topic', process_func=lambda v: 'ON' if v == '01' else 'OFF')
+전열교환기.register_command(message_flag='43', attr_name='mode', topic_class='mode_command_topic', process_func=lambda v: {'01': 'bypass', '03': 'heat'}.get(v, 'off')
+전열교환기.register_command(message_flag='42', attr_name='percentage', topic_class='percentage_command_topic', process_func=lambda v: {'01': '1', '02': '2', '03': '3'}.get(v, 'OFF')
 
 # 가스차단기
 optional_info = {'optimistic': 'false'}
